96bb723c80dc6d793ae9b581c73b4cce50d55e9d
dinesh5733 <ingoledinu01@gmail.com>
2024-11-22 22:42:24 +0530

    Modified the Code

---
+++ uberApp/src/main/java/com/din/project/uberApp/utils/GeometryUtil.java
@@ -0,0 +1,19 @@
+package com.din.project.uberApp.utils;
+
+import com.din.project.uberApp.dto.PointDTO;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
+
+public class GeometryUtil {
+
+    public static Point createPoint(PointDTO pointDto) {
+        GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
+        Coordinate coordinate = new Coordinate(pointDto.getCoordinates()[0],
+                pointDto.getCoordinates()[1]);
+
+        return geometryFactory.createPoint(coordinate);
+    }
+
+}
